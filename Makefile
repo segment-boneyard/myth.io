f17ac6f4c669d19757a8b826c25f07d003adf7a8
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-12-17 01:17:27 -0800

    revert to committing builds

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 
 build: node_modules components index.css
-	@./node_modules/.bin/component build --dev
+	@./node_modules/.bin/component build
 	@./node_modules/.bin/myth build/build.css build/build.css
 
 clean:
 	@rm -rf build components node_modules
 
 components: component.json
-	@./node_modules/.bin/component install --dev
+	@./node_modules/.bin/component install
 
 node_modules: package.json
 	@npm install
--- server.js
+++ server.js
@@ -1,54 +1,19 @@
 
-var app = require('./app');
-var Builder = require('component-builder');
 var express = require('express');
-var fs = require('fs');
-var write = fs.writeFileSync;
 var hbs = require('hbs');
-var Installer = require('component-installer');
-var myth = require('myth');
-var mkdir = require('mkdirp');
-
-/**
- * Environment.
- */
-
 var port = process.env.PORT || 7777;
-var dev = process.env.NODE_ENV != 'production';
-var dir = 'build';
 
 /**
- * Dev?
+ * App.
  */
 
-if (dev) return listen();
-
-/**
- * Install & build.
- */
-
-var installer = new Installer(__dirname);
-var builder = new Builder(__dirname);
-builder.copyFiles(true);
-builder.copyAssetsTo(dir);
-mkdir(dir);
-
-installer.install(function (err) {
-  if (err) throw err;
-  builder.build(function (err, res) {
-    if (err) throw err;
-    write(dir + '/build.js', res.require + res.js + res.templates);
-    write(dir + '/build.css', myth(res.css));
-    listen();
-  });
-});
-
-/**
- * Listen.
- */
-
-function listen () {
-  app.listen(port, function () {
+module.exports = express()
+  .engine('html', hbs.__express)
+  .set('views', __dirname)
+  .use('/build', express.static(__dirname + '/build'))
+  .get('*', function (req, res, next) {
+    res.render('index.html');
+  })
+  .listen(port, function () {
     console.log('Listening on port ' + port + '...');
   });
-}
